5e625eab0163074700fa5a5b36e181a5181703c4
Leandro Barral <leandro.obarral@gmail.com>
2024-10-28 14:38:13 -0300

    feat: removd disk health check, we don't save nothing in disk <3

--- src/modules/health/health.controller.ts
+++ src/modules/health/health.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
 import {
-  DiskHealthIndicator,
   HealthCheck,
   HealthCheckService,
   MemoryHealthIndicator,
@@ -13,7 +12,6 @@ import { RabbitMQHealthCheckService } from './rabbit-mq-health-check.service';
 export class HealthController {
   constructor(
     private readonly db: TypeOrmHealthIndicator,
-    private readonly disk: DiskHealthIndicator,
     private readonly health: HealthCheckService,
     private readonly memory: MemoryHealthIndicator,
     private readonly rabbitMQHealthCheck: RabbitMQHealthCheckService,
@@ -25,8 +23,6 @@ export class HealthController {
     return this.health.check([
       () => this.rabbitMQHealthCheck.check(),
       () => this.db.pingCheck('database'),
-      () =>
-        this.disk.checkStorage('storage', { path: '/', thresholdPercent: 0.5 }),
       () => this.memory.checkHeap('memory_heap', 150 * 1024 * 1024),
     ]);
   }
