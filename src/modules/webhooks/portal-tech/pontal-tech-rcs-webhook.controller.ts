da9865b7e32ba3e44e849ac8bf5766d478da4a8c
Leandro Barral <leandro@dev.>
2024-11-22 12:21:26 -0300

    fix: turning public webhook endpoint

--- src/modules/webhooks/portal-tech/pontal-tech-rcs-webhook.controller.ts
+++ src/modules/webhooks/portal-tech/pontal-tech-rcs-webhook.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
+import { PublicRoute } from '@/config/public-route';
 import { BrokerType, ChannelType } from '@/models/enums';
 import { PontalTechWebhookApiRequest } from '@/modules/brokers/pontal-tech/models/pontal-tech-rcs-webhook.model';
 import { InboundProducer } from '@/modules/message/producers/inbound.producer';
@@ -11,6 +12,7 @@ export class PontalTechRcsWebhookController {
   constructor(private readonly inboundProducer: InboundProducer) {}
 
   @Post('pontal-tech/rcs')
+  @PublicRoute()
   public async webhook(@Body() payload: PontalTechWebhookApiRequest) {
     this.inboundProducer.publish({
       channel: ChannelType.RCS,
